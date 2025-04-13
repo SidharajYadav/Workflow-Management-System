e9ffcae7a777086e80bdbc9af5444cb081cfeafd
SiddharajYadav <sidharajyadav157@gmail.com>
2025-04-13 09:35:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
   console.log(err.name, err.message);
   process.exit(1);
-});
+}); 
 
 connectDB();
 
